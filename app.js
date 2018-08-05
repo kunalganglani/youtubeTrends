ca0947b0ff950e3aa78ed1576c7c7e800626b22e
Kunal Ganglani <kunalganglani@gmail.com>
2018-08-05 15:36:03 +0530

    bootstrap select with search implemented

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap-select/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap-select/dist/css'));
 
 app.use('/', indexRouter);
 app.use('/youtube', youtubeRouter);
